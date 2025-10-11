bcc18deeed95297674631a99fc7b48982d8181c3
Your Name <your.email@example.com>
2025-10-11 09:43:10 +0900

    depuroy

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-export default defineConfig({
+export default defineConfig(({command}) => ({
+  base: command === 'serve' ? '/' : '/react-pokemon-zukan/',
   plugins: [
     react(),
     tailwindcss(),
   ],
-})
+}))
